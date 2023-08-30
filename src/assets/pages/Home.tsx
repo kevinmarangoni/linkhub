4683fa83ab791c14d226f929033465001c382724
kevinmarangoni <kevin-marangoni_@hotmail.com>
2023-08-30 08:56:46 -0300

    starts frontend for link shortner

--- src/assets/pages/Home.tsx
+++ src/assets/pages/Home.tsx
@@ -1,25 +1,105 @@
-import React,{useState} from 'react'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import {isValidUrl, copyToClipboard} from "@utils/url/url";
+import API from "@utils/api/api";
 
 const Home: React.FC = () => {
-  
+
   const [inputs, setInputs] = useState({
     long: "",
+    validURL: false,
     generated: "",
-  })
-  
-  return (
-    <div>
-      <div>
-        <input type={"text"} name={"long"} onChange={(e)=>{
-          setInputs({...inputs, [e.target.name]: e.target.value})
-        }}/>
-        <button type="button">Gerar link</button>
-      </div>
-      <div>
-        <input type={"text"} name={"generated"} value={inputs.generated} />
-      </div>
-    </div>
-  )
-}
+    clipboarded: false
+  });
 
-export default Home
+  useEffect(()=>{
+    let isValid = isValidUrl(inputs.long)
+    setInputs({
+      ...inputs,
+      validURL: isValid
+    })
+    console.log(inputs)
+  },[inputs.long, inputs.validURL])
+
+  async function handleGenerate(){
+    const shortened = await API.createShortLink(encodeURI(inputs.long))
+    if(shortened.ok){
+      setInputs({...inputs, generated: encodeURI(`${window.location.origin}/srt/${shortened.data.short}`)})
+    }
+  }
+
+  function handleClipboard(){
+    copyToClipboard(inputs.generated)
+    setInputs({
+      ...inputs, clipboarded: true
+    })
+  }
+
+  return (
+    <Container>
+      <Content>
+        <Form>
+          <h2>Encurtador de links</h2>
+          <div>
+            <input
+              type={"text"}
+              name={"long"}
+              onChange={(e) => {
+                setInputs({ ...inputs, [e.target.name]: e.target.value });
+              }}
+            />
+            <button type="button" disabled={!inputs.validURL} onClick={handleGenerate}>Gerar link</button>
+          </div>
+          {(!inputs.validURL && inputs.long.length != 0) && <p>link inválido</p>}
+          <div>
+            <input
+              type={"text"}
+              name={"generated"}
+              value={inputs.generated}
+              readOnly={true}
+            />
+            <button type="button" disabled={inputs.generated.length == 0} onClick={handleClipboard}>{inputs.clipboarded ? "Copiado!":"Copiar"}</button>
+          </div>
+        </Form>
+      </Content>
+    </Container>
+  );
+};
+
+export default Home;
+
+const Container = styled.section`
+  height: 100vh;
+  width: 100vw;
+`;
+const Content = styled.article`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  width: 100%;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  width: 400px;
+
+  h2 {
+
+  }
+  div {
+
+  }
+
+  input {
+    background-color: ${(props) => props.theme.background.secondary};
+  }
+
+  button {
+    background-color: ${(props) => props.theme.highlight};
+  }
+`;
--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -1,27 +1,60 @@
-import axios from 'axios'
+import axios from 'axios';
 
 export interface IRequestReturn {
-    ok: boolean,
-    message?: string | Object | any
-    data?: Object | any
+    ok: boolean;
+    message?: string | Object | any;
+    data?: IDataRequest | any;
 }
-class ApiMethods {
-    apiURL:string = "http://localhost:3001/api/v1"
 
-    async getLinkByShort(short: string): Promise<IRequestReturn> {
-        if(short == null || short == undefined || typeof short != 'string'){
-            return { ok: false, message: "invalid short" }
+export interface IDataRequest {
+    link: string,
+    short: string,
+    _id?: string,
+    createdAt?: Date,
+    updatedAt?: Date,
+    __v?: number | string
+}
+
+class ApiMethods {
+    apiURL: string = "http://localhost:3001/api/v1";
+    maxRetries: number = 5;
+    retryDelay: number = 1000; // Em milissegundos
+
+    async getLinkByShort(short: string, retryCount: number = 0): Promise<IRequestReturn> {
+        if (short == null || short == undefined || typeof short != 'string') {
+            return { ok: false, message: "invalid short" };
         }
-        try{
-            const response = await axios.get(`${this.apiURL}/link/short/${short}`)
-            return { ok: true, data: response.data }
-        }
-        catch(err:any){
-            console.log("Error at getLinkByShort, cause: " + err)
-            return { ok: false, message: err.message }
+        
+        try {
+            const response = await axios.get(`${this.apiURL}/link/short/${short}`);
+            return { ok: true, data: response.data };
+        } catch (err: any) {
+            console.log("Error at getLinkByShort, cause: " + err);
+            if (retryCount < this.maxRetries) {
+                await new Promise(resolve => setTimeout(resolve, this.retryDelay));
+                return this.getLinkByShort(short, retryCount + 1);
+            }
+            return { ok: false, message: err.message };
         }
     }
 
+    async createShortLink(link: string, retryCount: number = 0): Promise<IRequestReturn> {
+        try {
+            if (link == null || link == undefined || typeof link !== 'string') {
+                return { ok: false, message: "Invalid link parameter" };
+            }
+
+            const response = await axios.post(`${this.apiURL}/link`, { link });
+            return { ok: true, data: response.data };
+        } catch (err: any) {
+            console.log("Error at createShortLink, cause: " + err);
+            if (retryCount < this.maxRetries) {
+                await new Promise(resolve => setTimeout(resolve, this.retryDelay));
+                return this.createShortLink(link, retryCount + 1);
+            }
+            return { ok: false, message: err.message };
+        }
+    }
 }
 
 export default new ApiMethods()
